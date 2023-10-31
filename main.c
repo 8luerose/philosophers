b91c0fe69ee07ac2c62e649fb0488ace50fc433b
8luerose <8luerose@naver.com>
2023-10-31 22:34:32 +0900

    [MODIFYING]

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 21:12:27 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/10/31 22:18:24 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/10/31 22:22:35 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ int main(int ac, char **av)
 	t_philo		*philo;
 
 	if (ac != 5 && ac != 6)
-		return (print_error("Invalid arguments"));
+		return (print_error("Invalid arguments!!"));
 	memset(&arg, 0, sizeof(t_all_info));
 	if (init_arg(&arg, ac, av));
-		return (print_error("FAIL argument init"));
+		return (print_error("FAIL argument init!!"));
 	if (init_philo(&philo, &arg))
-		return (print_error("FAIL philo init"));
+		return (print_error("FAIL philo init!!"));
 	if (philosophers(&arg, philo))
-		return (print_error("FAIL philosophers eat"));
+		return (print_error("FAIL philosophers eat!!"));
 	return (0);
 }
--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 22:18:58 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/10/31 22:19:42 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/10/31 22:34:11 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,43 @@
 
 int philosophers(t_all_info *arg, t_philo *philo)
 {
+	int i;
 	
+	i = 0;
+	while (i < arg->philo_num)
+	{
+		philo[i].last_eat_clock = get_time();
+		if (pthread_create(&(philo[i].thread_id), NULL, make_thread, &(philo[i])))
+			return (FAIL);
+		i++;
+	}
 }
+
+// void *make_thread(void *arg)
+// {
+// 	t_philo *philo;
+
+// 	philo = (t_philo *)arg;
+// 	while (1)
+// 	{
+// 		if (philo->p_arg->must_eat_num != -1 && philo->eat_cnt >= philo->p_arg->must_eat_num)
+// 			break ;
+// 		if (philo->p_arg->philo_num % 2 == 1)
+// 		{
+// 			if (philo->id % 2 == 0)
+// 				eat(philo);
+// 			else
+// 				sleep(philo);
+// 		}
+// 		else
+// 		{
+// 			if (philo->id % 2 == 0)
+// 				sleep(philo);
+// 			else
+// 				eat(philo);
+// 		}
+// 	}
+// 	return (NULL);
+// }
+
+void *make_thread()
